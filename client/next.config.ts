7986f789c436cc0e9eb054b31110e0bae5ab2cd2
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-25 23:56:21 +0700

    chore: add external domain (supabase buckets)

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hiolnjzwfsnxkrocwgco.supabase.co",
+        pathname: "/storage/v1/object/public/assets/*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
