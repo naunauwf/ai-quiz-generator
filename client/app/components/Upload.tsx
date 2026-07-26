4507a27852a643126f4bf983ed7edb69059da79d
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-26 17:24:29 +0700

    feat: add component Upload

---
+++ client/app/components/Upload.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
+import { uploadImage } from "../utils/upload.data";
+
+export default function UploadImageButton() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+
+    if (!file) {
+      return null;
+    }
+
+    setLoading(true);
+    const url = await uploadImage(file, title, description);
+    setLoading(false);
+
+    if (url) {
+      alert("Gambar berhasil diupload dan disimpan ke Database!");
+      // automatically refresh the server data page
+      router.refresh();
+    } else {
+      alert("Gagal mengupload gambar");
+    }
+  }
+
+  return (
+    <main>
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        accept="image/*"
+        className="hidden"
+      />
+      <button
+        onClick={() => fileInputRef.current?.click()}
+        disabled={loading}
+        className="text-gray-900 w-20 h-7 leading-2 border-white border p-1 rounded-xl bg-green-300 text-[12px] font-bold m-2.5 text-center hover:bg-green-400 transition-all cursor-pointer disabled:opacity-50"
+      >
+        {loading ? "Uploading.." : "Upload File"}
+      </button>
+    </main>
+  );
+}
