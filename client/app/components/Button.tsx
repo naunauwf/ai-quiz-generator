28967c1b30707cdc2b6e9945da392295885e5dde
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-26 17:23:18 +0700

    feat: add button component

---
+++ client/app/components/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="text-gray-900 w-14 h-7 border-white border-2 p-1 rounded-xl bg-green-300 text-[12px] m-2.5 text-center hover:bg-green-400 transition-all hover:translate-y-px cursor-pointer font-bold">
+      Upload File
+    </button>
+  );
+}
