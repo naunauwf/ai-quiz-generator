d6e11e942e7ee42be8b5dbb8ffaf0e67366435aa
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-26 17:24:47 +0700

    feat: add dashboard page

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,37 @@
+import { getImages } from "../utils/get.data";
+import Card from "../components/Card";
+import Form from "../components/Form";
+
+export default async function Dashboard() {
+  const files = await getImages();
+
+  return (
+    <main>
+      <main className="p-5  bg-gray-900 h-auto w-[80%] m-auto">
+        <h1 className="text-center font-bold text-2xl sm:text-3xl text-white ">
+          List of images from Supabase
+        </h1>
+        {/* Form Input */}
+        <div>
+          <Form />
+        </div>
+        {/* show messages if the data is null */}
+        {(!files || files.length === 0) && (
+          <p className="p-5 text-center text-red-500 font-bold">
+            Tidak ada data gambar ditemukan pada tabel &apos;image_url&apos;
+          </p>
+        )}
+        <div className="flex justify-center items-center flex-wrap p-5 gap-5 sm:gap-3.5">
+          {files?.map((file) => (
+            <Card
+              key={file.id}
+              title={file.title}
+              description={file.description}
+              src={file.image_url}
+            />
+          ))}
+        </div>
+      </main>
+    </main>
+  );
+}
