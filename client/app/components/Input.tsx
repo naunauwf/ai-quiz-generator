fa51204e3d4bd0950a8416b148645e700d3cbdd2
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-28 02:15:06 +0700

    feat: add suppressHydrationWarning

--- client/app/components/Input.tsx
+++ client/app/components/Input.tsx
@@ -14,20 +14,19 @@ export default function Input({
   onChange,
 }: InputFieldProps) {
   return (
-    <div>
-      <div className="mb-4 sm:flex sm:flex-col sm:items-start">
-        <label className="block text-brand-dark font-bold mb-2 text-sm transition-all">
-          {label} {required && <span className="text-red-500">*</span>}
-        </label>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-          className="sm:w-96 px-3 py-1 border rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-dark text-brand-dark/70 transition-all placeholder:text-xs sm:placeholder:text-base focus:border-brand-dark outline-1 placeholder:text-brand-dark/30"
-          placeholder={placeholder}
-          required
-        />
-      </div>
+    <div className="mb-4 sm:flex sm:flex-col sm:items-start">
+      <label className="block text-brand-dark font-bold mb-2 text-sm transition-all">
+        {label} {required && <span className="text-red-500">*</span>}
+      </label>
+      <input
+        suppressHydrationWarning={true}
+        type="text"
+        value={value}
+        onChange={onChange}
+        className="sm:w-96 px-3 py-1 border rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-dark text-brand-dark/70 transition-all placeholder:text-xs sm:placeholder:text-base focus:border-brand-dark outline-1 placeholder:text-brand-dark/30"
+        placeholder={placeholder}
+        required
+      />
     </div>
   );
 }
