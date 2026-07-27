e48dcd698c14b05a562fb0f88922cfffa6f9b96c
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-27 20:43:18 +0700

    style: change some styles

--- client/app/components/Input.tsx
+++ client/app/components/Input.tsx
@@ -16,14 +16,14 @@ export default function Input({
   return (
     <main>
       <div className="mb-4 sm:flex sm:flex-col sm:items-start">
-        <label className="block text-green-300 font-bold mb-2 text-sm transition-all">
+        <label className="block text-brand-dark font-bold mb-2 text-sm transition-all">
           {label} {required && <span className="text-red-500">*</span>}
         </label>
         <input
           type="text"
           value={value}
           onChange={onChange}
-          className="sm:w-96 px-3 py-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300 text-taupe-50 transition-all placeholder:text-xs sm:placeholder:text-base"
+          className="sm:w-96 px-3 py-1 border rounded-lg focus:outline-none focus:ring-1 focus:ring-brand-dark text-brand-dark/70 transition-all placeholder:text-xs sm:placeholder:text-base focus:border-brand-dark outline-1 placeholder:text-brand-dark/30"
           placeholder={placeholder}
           required
         />
