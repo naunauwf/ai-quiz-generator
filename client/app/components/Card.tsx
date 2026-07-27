47af7bfc3779203b3ac11c863c2f23fcd8bd63d3
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-27 20:40:55 +0700

    feat: add extdended description

--- client/app/components/Card.tsx
+++ client/app/components/Card.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+import Notification from "./Notification";
 
 type CardProps = {
   title: string;
@@ -7,6 +10,11 @@ type CardProps = {
 };
 
 export default function Card({ title, description, src }: CardProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const MAX_LENGTH = 50;
+  const isLong = description.length > MAX_LENGTH;
+
   return (
     <main>
       <div className="w-60 h-80 rounded-xl overflow-hidden bg-white shadow-md">
@@ -24,9 +32,24 @@ export default function Card({ title, description, src }: CardProps) {
           <h2 className="text-lg font-bold mb-1 text-gray-900">
             {title || "untitled"}
           </h2>
-          <p className="text-gray-600 text-sm">
+          <p
+            className={`${expanded ? "Selengkapnya hidden" : "line-clamp-2"}  text-gray-700 text-sm  `}
+          >
             {description || "No description provided."}
           </p>
+          {isLong && (
+            <button
+              onClick={() => setExpanded(!expanded)}
+              className="text-sm text-gray-400 hover:text-gray-500"
+            >
+              {`${expanded ? "Tutup"  : "Selengkapnya"}`}
+            </button>
+          )}
+          {expanded && (
+            <Notification onClose={() => setExpanded(false)}>
+              {description}
+            </Notification>
+          )}
         </div>
       </div>
     </main>
