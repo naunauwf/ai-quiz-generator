e143aa3ffc355d608e7e77dd0a95963a484bb4dd
nauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-28 11:29:11 +0700

    feat:  show description button only on mobile, whereas on desktop the button appears only if the description is long

--- client/app/components/Card.tsx
+++ client/app/components/Card.tsx
@@ -16,41 +16,47 @@ export default function Card({ title, description, src }: CardProps) {
   const isLong = description.length > MAX_LENGTH;
 
   return (
-    <div>
-      <div className="w-37.5 h-56 sm:w-60 sm:h-80 rounded-xl overflow-hidden bg-white shadow-md">
-        <div className="relative w-full h-40 sm:h-52">
-          <Image
-            src={src}
-            alt={title || "image"}
-            className="w-full object-cover"
-            fill
-            loading="eager"
-            unoptimized
-          />
-        </div>
-        <div className="p-1.5 sm:p-3">
-          <h2 className="text-sm sm:text-lg font-bold text-gray-900">
-            {title || "untitled"}
-          </h2>
-          <p
-            className={`${expanded ? "Selengkapnya hidden" : "line-clamp-4"}  text-gray-700 sm:text-sm text-xs sm:h-10`}
+    <div className="w-37.5 h-56 sm:w-60 sm:h-80 rounded-xl overflow-hidden bg-white shadow-md">
+      <div className="relative w-full h-40 sm:h-52 ">
+        <Image
+          src={src}
+          alt={title || "image"}
+          className="w-full object-cover"
+          fill
+          loading="eager"
+          unoptimized
+        />
+      </div>
+      <div className="p-1 sm:p-3">
+        <h2 className="text-sm sm:text-lg font-bold text-gray-900">
+          {title || "untitled"}
+        </h2>
+        {/* description paragpah: hidden on mobile, show on desktop */}
+        <p className="hidden sm:block line-clamp-4 sm:line-clamp-2 sm:text-sm text-xs sm:h-10 text-gray-700">
+          {description || "No description provided."}
+        </p>
+        {/*the button that only show on mobile */}
+        <button
+          onClick={() => setExpanded(!expanded)}
+          className="block sm:hidden text-xs text-gray-600 hover:text-gray-700 cursor-pointer"
+        >
+          {`${expanded ? "Tutup" : "Description.."}`}
+        </button>
+        {/* button on desktop visible onnly if description is long */}
+        {isLong && (
+          <button
+            onClick={() => setExpanded(!expanded)}
+            className="text-sm hidden sm:block text-gray-400 hover:text-gray-500 cursor-pointer"
           >
-            {description || "No description provided."}
-          </p>
-          {isLong && (
-            <button
-              onClick={() => setExpanded(!expanded)}
-              className="text-sm text-gray-400 hover:text-gray-500"
-            >
-              {`${expanded ? "Tutup" : "Selengkapnya"}`}
-            </button>
-          )}
-          {expanded && (
-            <Notification onClose={() => setExpanded(false)}>
-              {description}
-            </Notification>
-          )}
-        </div>
+            {`${expanded ? "Tutup" : "Selengkapnya"}`}
+          </button>
+        )}
+        {/* popup modal */}
+        {expanded && (
+          <Notification onClose={() => setExpanded(false)}>
+            {description}
+          </Notification>
+        )}
       </div>
     </div>
   );
