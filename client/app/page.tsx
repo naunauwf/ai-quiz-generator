03e712ea149d14ca5aa8b611a6aea555f8f28bcf
naunauwf <199408064+naunauwf@users.noreply.github.com>
2026-07-30 15:31:23 +0700

    feat: change home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -22,24 +22,3 @@ export default async function Home() {
     </main>
   );
 }
-
-// {(!files || files.length === 0) && (
-//   <p className="p-5 text-center text-red-500 font-bold">
-//     Tidak ada data gambar ditemukan pada tabel &apos;image_url&apos;
-//   </p>
-// )}
-// <div className="flex gap-2.5 flex-wrap justify-center items-center p-5">
-//   {files?.map((file) => (
-//     // ambil public url untuk tiap file
-//     <Image
-//       key={file.id}
-//       src={file.image_url}
-//       alt="image"
-//       className="object-cover w-50 h-50"
-//       loading="eager"
-//       width={200}
-//       height={200}
-//       unoptimized
-//     />
-//   ))}
-// </div>
